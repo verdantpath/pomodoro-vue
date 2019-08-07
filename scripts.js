b17e32614effe6808c79aad42228f299591dfb00
verdantpath <m_dittmann@yahoo.com>
2019-08-06 19:45:11 -0700

    add random cat pic during rest"

--- scripts.js
+++ scripts.js
@@ -2,21 +2,57 @@ const POMODORO_STATES = {
   WORK: 'work',
   REST: 'rest'
 }
+const STATES = {
+  STARTED: 'started',
+  STOPPED: 'stopped',
+  PAUSED: 'paused'
+}
 const WORKING_TIME_LENGTH_IN_MINUTES = 25;
 const RESTING_TIME_LENGTH_IN_MINUTES = 5;
 
 new Vue({
   el: '#app',
   data: {
+    state: STATES.STOPPED,
     minute: WORKING_TIME_LENGTH_IN_MINUTES,
     second: 0,
-    pomodoroState: POMODORO_STATES.WORK,
+    pomodoroState: POMODORO_STATES.REST,
     timestamp: 0
+  },
+  computed: {
+    title: function() {
+      return this.pomodoroState === POMODORO_STATES.WORK ? 'Work' : 'Rest'
+    },
+    min: function() {
+      if (this.minute < 10) {
+        return '0' + this.minute;
+      }
+      return this.minute;
+    },
+    sec: function() {
+      if (this.second < 10) {
+        return '0' + this.second;
+      }
+      return this.second;
+    }
+
   },
   methods: {
     start: function() {
+      this.state = STATES.STARTED;
       this._tick();
       this.interval = setInterval(this._tick, 1000);
+    },
+    pause: function() {
+      this.state = STATES.PAUSED;
+      clearInterval(this.interval);
+    },
+    stop: function() {
+      this.state = STATES.STOPPED;
+      clearInterval(this.interval);
+      this.pomodoroState = POMODORO_STATES.WORK;
+      this.minute = WORKING_TIME_LENGTH_IN_MINUTES;
+      this.second = 0;
     },
     _tick: function() {
       // if second is not 0, just decrement second
